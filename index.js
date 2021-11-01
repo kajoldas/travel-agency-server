4a07eb3536331cfb0c7c04849fcd88438d5188f4
kajoldas <kajol.nwu@gmail.com>
2021-11-01 16:45:38 +0600

    added port

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
 const cors = require('cors')
 require('dotenv').config()
+const { MongoClient } = require('mongodb');
+const ObjectId = require("mongodb").ObjectId;   
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -14,39 +15,38 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-
-
 async function run(){
      try{
         await client.connect();
-        // console.log('database connected successfully');
-
+        console.log('database connected successfully')
         const database = client.db('travelAgency');
         const servicesCollection = database.collection('services');
-    
-        //-------------GET Products API
-        // app.get('/places', async (req, res) => {
-        //     const cursor = servicesCollection.find({});
-        //     const places = await cursor.toArray();
-        //     // const count = await cursor.count();
-        //     res.send({places});
-        // })
-
-
+        
+        //-------------GET Products API-----------
+        app.get('/places', async (req, res) => {
+            const cursor = servicesCollection.find({});
+            const places = await cursor.toArray();
+            // const count = await cursor.count();
+            res.send(places);
+        })
         //--------------POST API-------------
-        app.post('/services', async(req, res) => {
-      
-            
-            const services = req.body;
-            console.log('hit the post api', services);
+        app.post('/places', async(req, res) => {
+            const place = req.body;
+            console.log('hit the post API', place)
+            const result = await servicesCollection.insertOne(place);
+            console.log(result)
+            res.json(result)
+        })
+
+        //-----------------DELETE API-----------------
+        app.delete('/places/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await servicesCollection.deleteOne(query)
+            console.log('delete place ', result);
+            res.json(result)
            
-            const result = await servicesCollection.insertOne(services);
-            console.log(result);
-            res.json(result);
-        });
-
-
-
+        })
      }
      finally{
         //  await client.close();
@@ -56,12 +56,9 @@ async function run(){
 run().catch(console.dir);
 
 
-
-
 app.get('/', (req, res) => {
     res.send('Server is running')
 })
-
 app.listen(port, () => {
     console.log('Server running on port', port)
 })
